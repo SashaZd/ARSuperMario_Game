c23e28107ffd9cb699a71147be519fc078b744fd
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2015-10-11 20:46:58 -0500

    Changed Goomba to a paper enemy

--- Assets/Scripts/Level Parts/Paper.cs
+++ Assets/Scripts/Level Parts/Paper.cs
@@ -3,12 +3,17 @@ using System.Collections;
 
 // An enemy that will kill the player on contact.
 // Landing on it from above will kill it instead.
-public class Goomba : Enemy {
+public class Paper : Enemy {
+
+	// Update is called once per frame.
+	void Update () {
+		transform.Rotate (new Vector3 (0f, 8f, 0f));
+	}
 
 	// Fires when the goomba hits the player.
 	void OnCollisionEnter (Collision collision) {
 		if (collision.collider.tag == "Player") {
-			// Check if the player is high enough to "Goomba stomp" the enemy.
+			// Check if the player is high enough to flatten the enemy.
 			float playerStompHeight = collision.collider.transform.position.y - collision.collider.bounds.extents.y / 2;
 			float enemyHeadHeight = transform.position.y + GetComponent<Collider> ().bounds.extents.y * 0.45f;
 			if (playerStompHeight > enemyHeadHeight || collision.collider.transform.GetComponent<Rigidbody> ().velocity.y < -0.01f) {
