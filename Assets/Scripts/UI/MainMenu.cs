479ba058a679c9f0d9c1414b1ec6e62394077676
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2016-02-06 23:41:15 -0500

    Added a primitive UI

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+// Button listeners for the main menu.
+public class MainMenu : MonoBehaviour {
+
+	// The name of the scene that the scanning button will take the user to.
+	public string startScene = "";
+
+	// Should normally allow the user to start scanning the environment with the Kinect.
+	// Currently just takes the user directly to the first screen.
+	public void StartScan() {
+		SceneManager.LoadScene (startScene);
+	}
+}
