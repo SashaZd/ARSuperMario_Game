aa6e50f83f3285a498592618af42f9560a295888
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2016-02-19 16:34:33 -0500

    Added HTTP post support

--- Assets/Scripts/Managers/NetworkingManager.cs
+++ Assets/Scripts/Managers/NetworkingManager.cs
@@ -15,14 +15,29 @@ public class NetworkingManager : MonoBehaviour {
 	}
 
 	// Processes a string obtained from a URL.
-	public void ProcessStringFromURL (String url, Action<string> OnGet) {
-		StartCoroutine(FetchURL (url, OnGet));
+	public void ProcessStringFromURL (string url, Action<string> OnGet) {
+		StartCoroutine(GetURL (url, OnGet));
 	}
 
 	// Processes a string obtained from a URL.
-	private IEnumerator FetchURL (String url, Action<string> OnGet) {
+	private IEnumerator GetURL (string url, Action<string> OnGet) {
 		WWW www = new WWW(url);
 		yield return www;
 		OnGet(www.text);
+	}
+
+	// Posts a position to a URL.
+	public void PostPositionInURL (string url, Vector3 position) {
+		WWWForm form = new WWWForm();
+		form.AddField ("x", position.x.ToString ());
+		form.AddField ("y", position.y.ToString ());
+		form.AddField ("z", position.z.ToString ());
+		PostURL (url, form);
+	}
+
+	// Posts a form to a URL.
+	private IEnumerator PostURL (string url, WWWForm form) {
+		WWW www = new WWW(url, form);
+		yield return www;
 	}
 }
